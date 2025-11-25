849c830417e2b7cab29303c4624b41bbe9b10118
gusttavosants <gustavoluizsantosmelo@gmail.com>
2025-11-25 18:41:02 -0300

    Criacao das entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
+import { ControllerModule } from './controller/controller.module';
+import { CollaboratorModule } from './collaborator/collaborator.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule, EnrollmentModule, ControllerModule, CollaboratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
